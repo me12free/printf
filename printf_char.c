b2240d549601a12bced878f10e82b5f952f9e9f1
me12free <johnewoi72@gmail.com>
2023-03-27 10:46:57 -0700

    printf

--- printf_char.c
+++ printf_char.c
@@ -1,35 +1,14 @@
 #include "main.h"
-
 /**
- * printf_bin - prints a binary number.
+ * printf_char - prints a char.
  * @val: arguments.
  * Return: 1.
  */
-
-int printf_bin(va_list val)
+int printf_char(va_list val)
 {
-	int flag = 0;
-	int cont = 0;
-	int i, a = 1, b;
-	unsigned int num = va_arg(val, unsigned int);
-	unsigned int p;
+	char s;
 
-	for (i = 0; i < 32; i++)
-	{
-		p = ((a << (31 - i)) & num);
-		if (p >> (31 - i))
-			flag = 1;
-		if (flag)
-		{
-			b = p >> (31 - i);
-			_putchar(b + 48);
-			cont++;
-		}
-	}
-	if (cont == 0)
-	{
-		cont++;
-		_putchar('0');
-	}
-	return (cont);
+	s = va_arg(val, int);
+	_putchar(s);
+	return (1);
 }
