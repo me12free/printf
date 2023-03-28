a52ca21a1d5468b6bc7db946e803d85ad0cb0550
NoniDorcas <kdorcasmuthoni@gmail.com>
2023-03-28 01:08:17 -0700

    printf

--- main.h
+++ main.h
@@ -7,6 +7,19 @@
 #include <limits.h>
 #include <unistd.h>
 
+/**
+ * struct format - matches the conversion specifiers for printf
+ * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier
+ *
+ */
+
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} convert_match;
+
 int _putchar(char c);
 int printf_37(void);
 int printf_HEX(va_list val);
@@ -24,5 +37,6 @@ int printf_rot13(va_list args);
 int printf_srev(va_list args);
 int printf_string(va_list val);
 int printf_unsigned(va_list args);
+int _printf(const char *format, ...);
 
 #endif
