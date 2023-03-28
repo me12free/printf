56cf8d9c6994c0d7ebd0fc14d9ac93aa646f45a1
NoniDorcas <kdorcasmuthoni@gmail.com>
2023-03-28 01:36:13 -0700

    printf

--- main.h
+++ main.h
@@ -41,5 +41,7 @@ int printf_srev(va_list args);
 int printf_string(va_list val);
 int printf_unsigned(va_list args);
 int _printf(const char *format, ...);
+int _strlen(char *s);
+int _strlenc(const char *s);
 
 #endif
